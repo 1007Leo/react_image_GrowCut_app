129d6b8e6055c95a1f90671799b8ff36f46b1884
Alexey Chentsov <45707643+1007Leo@users.noreply.github.com>
2023-11-07 13:17:36 +0300

    Initial commit

---
+++ src/components/app.jsx
@@ -0,0 +1,12 @@
+import {useEffect, useState} from "react";
+
+function App(){
+
+    return(
+        <main className='main'>
+            <h1>Hello world!</h1>
+        </main>
+    )
+}
+
+export {App};
