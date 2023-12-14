9ff9dac080b9602204e41b2628acde17b90231d9
Alexey Chentsov <45707643+1007Leo@users.noreply.github.com>
2023-12-14 16:18:37 +0300

    Fixed opencv.js not loading

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -29499,7 +29499,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49918" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61514" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
